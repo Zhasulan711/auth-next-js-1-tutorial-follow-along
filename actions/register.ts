852595041d31b6861bf4ac308416eadd9bedfbc4
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-03-18 09:27:44 +0500

    create User

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,11 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
+
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const Register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -12,7 +16,29 @@ export const Register = async (values: z.infer<typeof RegisterSchema>) => {
     };
     // return Response.json("");    for API
   }
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return {
+      error: "Email already in use!",
+    };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  // TODO: Send verification email
+
   return {
-    success: "Email sent!",
+    success: "User created!",
   };
 };
