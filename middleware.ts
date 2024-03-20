1293059bafb298d9c2011e37969d6cb748087e71
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-03-20 18:46:15 +0500

    fixing middleware

--- middleware.ts
+++ middleware.ts
@@ -26,19 +26,19 @@ export default auth((req) => {
 
   if (isApiAuthRoute) {
     // return new Response(null, { status: 200 });
-    return null;
+    return;
   }
   if (isAuthRoute) {
     if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
     // return new Response(null, { status: 200 });
-    return null;
+    return;
   }
   if (!isLoggedIn && !isPublicRoute) {
     return Response.redirect(new URL("auth/login", nextUrl));
   }
-  return null;
+  return;
 });
 
 // Optionally, don't invoke Middleware on some paths
