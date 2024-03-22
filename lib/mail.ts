974a324fcadb15dd686ae2b736e629f20fc7e0ed
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-03-22 20:44:13 +0500

    email verification

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3000/auth/new-verification/token=${token}`;
+  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
     from: "onboarding@resend.dev",
@@ -12,3 +12,4 @@ export const sendVerificationEmail = async (email: string, token: string) => {
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
   });
 };
+  
