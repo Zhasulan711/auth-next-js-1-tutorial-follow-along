97b57f2535104ab3e880a92e4035cbd4abdd5c24
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-03-24 06:10:32 +0500

    fixing role in admin.ts

--- actions/admin.ts
+++ actions/admin.ts
@@ -1,11 +1,12 @@
 "use server";
 
 import { currentRole } from "@/lib/auth";
+import { UserRole } from "@prisma/client";
 
 export const admin = async () => {
   const role = await currentRole();
 
-  if (role === "ADMIN") {
+  if (role === UserRole.ADMIN) {
     return {
       success: "Allowed Server Action!",
     };
