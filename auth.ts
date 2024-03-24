17dc238198c2f0a5df0ac990fb437bb9edf35486
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-03-24 09:08:14 +0500

    Settings Page

--- auth.ts
+++ auth.ts
@@ -7,6 +7,7 @@ import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
 import { getUserById } from "@/data/user";
 import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
+import { getAccountByUserId } from "./data/account";
 
 export const {
   handlers: { GET, POST },
@@ -80,12 +81,19 @@ export const {
         session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
       }
 
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email as string;
+        session.user.isOAuth = token.isOAuth as boolean;
+      }
+
       return session;
     },
 
     async jwt({ token }) {
       // console.log({ token });
       // token.customField = "test";
+      // console.log("I AM BEING CALLED AGAIN!");
 
       if (!token.sub) {
         return token;
@@ -97,6 +105,11 @@ export const {
         return token;
       }
 
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
       token.role = existingUser.role;
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
